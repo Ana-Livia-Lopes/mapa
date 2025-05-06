b70dda4cd8b2df39a9e2c64c7336035610ebcf7b
Ana-Livia-Lopes <ana.livia.santos.lopes@gmail.com>
2025-05-06 11:46:18 -0300

    inicio

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Ana Lívia dos Santos Lopes nº1 DS
+// Isadora Gomes da Silva nº 9
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import Home from './src/screens/home';
+import Perfil from './src/screens/perfil';
+import Login from './src/screens/login';
+
+
+const Drawer = createDrawerNavigator();
+
+
+const App = () => (
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName='Login' >
+          <Drawer.Screen name="Home" component={Home} />
+          <Drawer.Screen name="Perfil" component={Perfil} />
+          <Drawer.Screen name="Login" component={Login} options={{ headerShown: false, drawerItemStyle: { display: 'none' } }}/>
+        </Drawer.Navigator>
+      </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  export default App;
